f4245375bbeacbd9a615ca3621f1dd0e76097251
rigel <rigel.marcinik@springcm.com>
2025-03-04 15:42:47 -0600

    godot version update

--- scripts/portal.gd
+++ scripts/portal.gd
@@ -14,21 +14,14 @@ func _ready():
 	_set_viewport_mat($Enter/MeshInstance3D, $Exit/ExitView)
 	_set_viewport_mat($Exit/MeshInstance3D, $Enter/EnterView)
 
-# https://old.reddit.com/r/godot/comments/13d93o1/godot_4_viewport_texture_error/
-# avoids false error if the viewport texture is set in code
-func _set_viewport_mat(_display_mesh: MeshInstance3D, _sub_viewport: SubViewport, _surface_id: int = 0):
-	var _mat: StandardMaterial3D = StandardMaterial3D.new()
-	var viewport_texture = _sub_viewport.get_texture()
-	_mat.albedo_texture = viewport_texture
-	_mat.emission_enabled = true
-	_mat.emission_texture = viewport_texture
-	_mat.emission_energy_multiplier = 1.0  # Adjust this value to control brightness
-	_display_mesh.set_surface_override_material(_surface_id, _mat)
-
 func _on_body_entered(body: PhysicsBody3D, exit: Node3D):
 	if body.is_in_group("Player") and _timer.is_stopped():
 		_timer.start()
 		body.global_position = exit.global_position
+
+func _process(_delta: float) -> void:
+	portal_mover()
+
 
 func portal_mover():
 	var viewport = get_viewport()
@@ -43,5 +36,13 @@ func portal_mover():
 	_entercam.basis = _exit.basis.rotated(Vector3.UP, PI)
 	_exitcam.basis = _enter.basis.rotated(Vector3.UP, PI)
 
-func _process(_delta: float) -> void:
-	portal_mover()
+# https://old.reddit.com/r/godot/comments/13d93o1/godot_4_viewport_texture_error/
+# avoids false error if the viewport texture is set in code
+func _set_viewport_mat(_display_mesh: MeshInstance3D, _sub_viewport: SubViewport, _surface_id: int = 0):
+	var _mat: StandardMaterial3D = StandardMaterial3D.new()
+	var viewport_texture = _sub_viewport.get_texture()
+	_mat.albedo_texture = viewport_texture
+	_mat.emission_enabled = true
+	_mat.emission_texture = viewport_texture
+	_mat.emission_energy_multiplier = 1.0  # Adjust this value to control brightness
+	_display_mesh.set_surface_override_material(_surface_id, _mat)
